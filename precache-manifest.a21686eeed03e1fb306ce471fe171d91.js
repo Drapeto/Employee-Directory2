2fff83e2e15be06c89310378024e2c3023d91d9e
Drapeto <drapetorres@gmail.com>
2020-07-22 23:45:02 -0700

    Updates

---
+++ precache-manifest.a21686eeed03e1fb306ce471fe171d91.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2d90b60b248aa7f14f6a555c4744d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8364cb6cec8313dbf5e5",
+    "url": "/static/js/main.ddded395.chunk.js"
+  },
+  {
+    "revision": "1a9355e74d03dc4c7068",
+    "url": "/static/js/runtime-main.e0225f3c.js"
+  }
+]);
---
+++ static/js/main.ddded395.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdirectory=this.webpackJsonpdirectory||[]).push([[0],[function(o,t,c){o.exports=c(1)},function(o,t){}],[[0,1]]]);
+//# sourceMappingURL=main.ddded395.chunk.js.map
